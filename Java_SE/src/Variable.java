3226e11f9c6dbfc5834bf27421f5746792a8c9e0
Kostya70 <Mirror50s@mail.ru>
2023-01-19 19:15:36 +0500

    add lesson

---
+++ Java_SE/src/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        int days = 365;
+        int years = days / 365;
+        int months = (days - (years * 365)) / 30;
+        int leftDays = days - (years * 365) - (months * 30);
+        System.out.println(years);
+        System.out.println(months);
+        System.out.println(leftDays);
+
+    }
+}
---
+++ Java_SE/src/WhatToEat.java
@@ -0,0 +1,14 @@
+public class WhatToEat {
+    public static void main(String[] args) {
+        int money = 8;
+        if (money > 10) {
+            System.out.println("Pizza");
+        } else if (money > 6) {
+            System.out.println("hamburger");
+            
+        }else {
+            System.out.println("sandwich");
+        }
+
+    }
+}
