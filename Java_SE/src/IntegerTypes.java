d10fadbdc730f6c8cd832e1a29c99b97505dbcd8
Kostya70 <Mirror50s@mail.ru>
2023-01-19 20:40:03 +0500

    add lesson integer type

---
+++ Java_SE/src/IntegerTypes.java
@@ -0,0 +1,12 @@
+public class IntegerTypes {
+    public static void main(String[] args) {
+//        long speed = 300_000;
+//        long distance = 365 * 24 * 60 * 60 * speed;
+//        System.out.println(distance);
+// Приведение типов
+//        byte b = 10;
+//        b = (byte) (b + 10);
+//        System.out.println(b);
+
+    }
+}
